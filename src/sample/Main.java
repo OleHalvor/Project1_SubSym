05b980a1a6fa7c35266eb3026244e21fcc9819f6
olehalvor <olli408@hotmail.com>
2016-01-22 12:09:58 +0100

    blabla

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,59 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.application.Platform;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.util.concurrent.CountDownLatch;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+public class Main extends Application {
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    GraphicsContext gc;
+    Canvas canvas;
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Drawing Operations Test");
+        Group root = new Group();
+        canvas = new Canvas(300, 250);
+        gc = canvas.getGraphicsContext2D();
+        drawShapes(gc);
+        root.getChildren().add(canvas);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+
+        Update update = new Update();
+        update.setDaemon(true);
+        System.out.println("Starting background task...");
+        update.start();
+
+    }
+
+    public void repaint(){
+
+    }
+
+    private void drawShapes(GraphicsContext gc) {
+        gc.setFill(Color.GREEN);
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(5);
+        gc.fillOval(Values.getX(), Values.getY(), 30, 30);
+    }
+
+
 }
---
+++ src/sample/Values.java
@@ -0,0 +1,14 @@
+package sample;
+
+/**
+ * Created by Ole on 21.01.2016.
+ */
+public class Values {
+    static int x = 10;
+    static int y = 20;
+
+    public static int getX(){return x;}
+    public static int getY(){return x;}
+    public static void setX(int x1){x = x1;}
+    public static void setY(int y1){y = y1;}
+}
