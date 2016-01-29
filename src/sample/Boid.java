0a318081f5da5cd87293104de06a7fde0f489c3c
andlon93 <andreas.lones@gmail.com>
2016-01-29 11:52:04 +0100

    revised the three rules

--- src/sample/Boid.java
+++ src/sample/Boid.java
@@ -7,84 +7,69 @@ public class Boid {
     private int x;
     private int y;
     private int dir;
-    private double velocityX;
-    private double velocityY;
+    private double velocity;
     private boolean alive;
 
-    public Boid(int x, int y, int dir, double velocityX, double velocityY){
+    public Boid(int x, int y, int dir, double velocity){
         this.x = x;
         this.y = y;
         this.dir = dir;
-        this.velocityX = velocityX;
-        this.velocityY = velocityY;
+        this.velocity = velocity;
         this.alive = true;
     }
 
     /* --  Start Boid Rules  -- */
-    private ArrayList<Double> rule1(Boid[] boids){
-        double x = 0;
-        double y = 0;
+    private ArrayList<Double> cohesion(Boid[] boids){
+        double goalX = 0;
+        double goalY = 0;
 
         for (Boid b : boids){
-            if (!b.equals(this)){
-                x = x + b.getx();
-                y = y + b.gety();
-            }
+            goalX = goalX + b.getx();
+            goalY = goalY+ b.gety();
         }//end Forloop
 
-        x = x / (boids.length - 1);
-        y = y / (boids.length - 1);
-
-        x = (x - this.getx())/10;
-        y = (y - this.gety())/10;
+        goalX = goalX / (boids.length);
+        goalY = goalY / (boids.length);
 
 
         ArrayList<Double> vector = new ArrayList<Double>(2);
-        vector.add(x);
-        vector.add(y);
-
+        vector.add(goalX);
+        vector.add(goalY);
         return vector;
     }
 
-    private ArrayList<Double> rule2(Boid[] boids){
-        double x = 0;
-        double y = 0;
+    private ArrayList<Double> separation(Boid[] boids){
+        double goalX = 0;
+        double goalY = 0;
+
         for (Boid b : boids){
-            if (!b.equals(this)){
-                int diffX = Math.abs(b.getx()-this.getx());
-                int diffY = Math.abs(b.gety()-this.gety());
-                if (Math.sqrt(Math.pow(diffX, 2)+Math.pow(diffY,2)) < 100){
-                    x = x - (b.getx() - this.getx());
-                    y = y - (b.getx() - this.gety());
-                }
-            }
-        }//End Forloop
+            goalX = goalX + this.getx() - b.getx();
+            goalY = goalY + this.gety() - b.gety();
+        }
+
+        goalX = goalX/boids.length;
+        goalY = goalY/boids.length;
 
         ArrayList<Double> vector = new ArrayList<Double>(2);
-        vector.add(x);
-        vector.add(y);
+        vector.add(goalX);
+        vector.add(goalY);
 
         return vector;
     }
 
-    private ArrayList<Double> rule3(Boid[] boids){
-        double x = 0;
-        double y = 0;
+    private ArrayList<Double> alignment(Boid[] boids){
+        double dDir = 0;
+        double dVel = 0;
         for (Boid b : boids) {
-            if (!b.equals(this)) {
-                x = x + b.getVelocityX();
-                y = y + b.getVelocityY();
-            }
+            dDir = dDir + b.getdir() - this.getdir();
+            dVel = dVel + b.getVelocity() - this.getVelocity();
         }//End forloop
-        x = x / (boids.length - 1);
-        y = y / (boids.length - 1);
-
-        x = (x - this.getVelocityX()) / 8;
-        y = (y - this.getVelocityY()) / 8;
+        dDir = dDir/boids.length;
+        dVel = dVel/boids.length;
 
         ArrayList<Double> vector = new ArrayList<Double>(2);
-        vector.add(x);
-        vector.add(y);
+        vector.add(dDir);
+        vector.add(dVel);
 
         return vector;
     }
@@ -92,22 +77,11 @@ public class Boid {
 
     /* -- This method executes all the rules of a boid -- */
     public void executeRules(Boid[] boids, double w1, double w2, double w3){
-        ArrayList<Double> rule1 = rule1(boids);
-        ArrayList<Double> rule2 = rule2(boids);
-        ArrayList<Double> rule3 = rule3(boids);
+        ArrayList<Double> cohesion = cohesion(boids);
+        ArrayList<Double> separation = separation(boids);
+        ArrayList<Double> alignment = alignment(boids);
 
-        this.setVelocityX(this.getVelocityX()+(rule1.get(0)*w1)+(rule2.get(0)*w2)+(rule3.get(0)*w3));
-        this.setVelocityY(this.getVelocityY()+(rule1.get(1)*w1)+(rule2.get(1)*w2)+(rule3.get(1)*w3));
 
-        double vLim = 7;
-        double Velocity = Math.sqrt(Math.pow(this.getVelocityX(),2)+Math.pow(this.getVelocityY(),2));
-        if (Velocity > vLim){
-            this.setVelocityX((this.getVelocityX()/Velocity)*vLim);
-            this.setVelocityY((this.getVelocityY()/Velocity)*vLim);
-        }
-
-        this.setX(this.getx()+(int)this.getVelocityX());
-        this.setY(this.gety()+(int)this.getVelocityY());
     }
 
     /* -- Getters and Setters -- */
@@ -117,8 +91,6 @@ public class Boid {
     public void setY(int y) {this.y = y;}
     public int getdir(){return dir;}
 
-    public double getVelocityX(){return velocityX;}
-    public double getVelocityY(){return velocityY;}
-    public void setVelocityX(double velocityX) {this.velocityX = velocityX;}
-    public void setVelocityY(double velocityY) {this.velocityY = velocityY;}
+    public double getVelocity(){return velocity;}
+    public void setVelocity(double velocity) {this.velocity = velocity;}
 }
