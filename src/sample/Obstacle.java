1368cff5c5f58104db0c977a13696e52dede7cbe
olehalvor <olli408@hotmail.com>
2016-02-01 16:07:36 +0100

    Basic obstacle and predator classes

--- src/sample/Obstacle.java
+++ src/sample/Obstacle.java
@@ -4,4 +4,32 @@ package sample;
  * Created by Ole on 01.02.2016.
  */
 public class Obstacle {
+
+    private int x;
+    private int y;
+    private double radius;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 }
--- src/sample/Predator.java
+++ src/sample/Predator.java
@@ -4,4 +4,39 @@ package sample;
  * Created by Ole on 01.02.2016.
  */
 public class Predator {
+    private int x;
+    private int y;
+    private double velocityX, velocityY;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double getVelocityX() {
+        return velocityX;
+    }
+
+    public void setVelocityX(double velocityX) {
+        this.velocityX = velocityX;
+    }
+
+    public double getVelocityY() {
+        return velocityY;
+    }
+
+    public void setVelocityY(double velocityY) {
+        this.velocityY = velocityY;
+    }
 }
